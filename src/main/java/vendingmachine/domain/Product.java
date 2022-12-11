41e834f98ad3b2770790bf2b15041b898c5b5415
juno-junho <ssmm0205@naver.com>
2022-12-11 14:48:19 +0900

    feat(domain): Product에 getter 및 amount 감소 메서드 추가

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -12,7 +12,23 @@ public class Product {
 
     public Product(List<String> productList) {
         this.name = productList.get(0);
-        this.price = Util.convertStringToPrice(productList.get(1));
+        this.price = Util.convertStringToProductPrice(productList.get(1));
         this.amount = Util.convertStringToInt(productList.get(2));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void decreaseAmount() {
+        this.amount -= 1;
     }
 }
