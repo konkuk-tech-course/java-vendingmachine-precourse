7b03b8975560027a159b5e3a12a97a5ee91ca622
yummygyudon <bang2brew@gmail.com>
2022-12-11 12:24:26 +0900

    FEAT(model$validator) : 금액 유효성 검사 기능 구현
    - PriceValidator 클래스
      - validateIsDigit 메서드 : 숫자인지 검사
      - validateCanDividedByTen 메서드 : 10으로 나눠지는 값인지 검사

--- src/main/java/vendingmachine/model/validator/PriceValidator.java
+++ src/main/java/vendingmachine/model/validator/PriceValidator.java
@@ -1,8 +1,24 @@
 package vendingmachine.model.validator;
 
+import vendingmachine.model.validator.util.ValidateErrors;
+
 public class PriceValidator extends Validator{
     @Override
-    void validate(String inputValue) {
+    public void validate(String inputValue) {
         super.validate(inputValue);
+        String trimValue = inputValue.trim();
+        validateIsDigit(trimValue);
+        validateCanDividedByTen(trimValue);
+    }
+
+    private void validateIsDigit(String inputValue){
+        if (!inputValue.matches("\\d")) {
+            throw new IllegalArgumentException(ValidateErrors.NOT_NUMBER.getMessage());
+        }
+    }
+    private void validateCanDividedByTen(String inputValue){
+        if(Integer.parseInt(inputValue)%10 != 0){
+            throw new IllegalArgumentException(ValidateErrors.UNABLE_DIVIDED.getMessage());
+        }
     }
 }
--- src/main/java/vendingmachine/model/validator/util/ValidateErrors.java
+++ src/main/java/vendingmachine/model/validator/util/ValidateErrors.java
@@ -1,7 +1,9 @@
 package vendingmachine.model.validator.util;
 
 public enum ValidateErrors {
-    BLANK("[ERROR] 공백은 허용되지 않는다.");
+    BLANK("[ERROR] 공백은 허용되지 않는다."),
+    NOT_NUMBER("[ERROR] 이름은 5자 이하여야 한다."),
+    UNABLE_DIVIDED("[ERROR] 금액은 10원으로 나누어져야 합니다.");
 
     private final String message ;
 
