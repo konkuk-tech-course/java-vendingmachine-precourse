adc2f064ab398d4b6cef2e2d605d00af64e3fe67
khyojun <nandong5923@naver.com>
2022-12-11 10:32:30 +0900

    feat(vending): 초기 패키지 클래스 구조 생성

---
+++ src/main/java/vendingmachine/controller/VendingController.java
@@ -0,0 +1,20 @@
+package vendingmachine.controller;
+
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class VendingController {
+
+    private InputView inputView;
+    private OutputView outputView;
+
+    public VendingController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+
+
+
+
+}
