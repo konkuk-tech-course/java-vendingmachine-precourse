a7d17e50880abffb239ea6fae6257abeb0c02514
yummygyudon <bang2brew@gmail.com>
2022-12-11 12:18:01 +0900

    FEAT(model$generator) : 자판기 기본 금액 세팅 기능 구현
    - MachineCoinGenerator 클래스
    - Map<Coin, Integer> 객체 반환
      - 금액대별 몫 값 저장

--- src/main/java/vendingmachine/model/generator/MachineCoinGenerator.java
+++ src/main/java/vendingmachine/model/generator/MachineCoinGenerator.java
@@ -1,23 +1,37 @@
 package vendingmachine.model.generator;
 
-import java.util.HashMap;
 import java.util.Map;
 import vendingmachine.model.Coin;
 import vendingmachine.model.generator.util.RandomAmountGenerator;
 
 public class MachineCoinGenerator {
     private static final RandomAmountGenerator amountGenerator = new RandomAmountGenerator();
+    private static final Map<Coin, Integer> COIN_TABLE = Map.of(
+            Coin.COIN_500, 0,
+            Coin.COIN_100, 0,
+            Coin.COIN_50, 0,
+            Coin.COIN_10, 0
+    );
+    private int inputMoney;
 
-    public Map<Coin, Integer> settingMachineCoins(int inputMoney) {
-        Map<Coin, Integer> coinTable = new HashMap<>();
-        int divideAmount;
-        int divideResult;
+    public MachineCoinGenerator(int inputMoney){
+        this.inputMoney = inputMoney;
+    }
+
+    public Map<Coin, Integer> settingMachineCoins() {
         do {
+            int divideAmount = amountGenerator.generate();
+            int amountCount = divideMoney(divideAmount);
+            int oldValue = COIN_TABLE.get(Coin.by(divideAmount));
+            COIN_TABLE.replace(Coin.by(divideAmount), oldValue+amountCount);
+            updateInputMoney(divideAmount, amountCount);
         } while (inputMoney > 0);
-        return coinTable;
+        return COIN_TABLE;
     }
-    private int divideMoney(){
-        return 0;
+    private int divideMoney(int amount){
+        return inputMoney/amount;
     }
-
+    private void updateInputMoney(int amount, int count){
+        this.inputMoney -= (amount*count);
+    }
 }
