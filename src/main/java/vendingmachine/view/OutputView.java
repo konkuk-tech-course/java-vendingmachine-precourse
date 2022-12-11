6cc5d4d83150fd90a1a5c96c3b9dd3acb21e4fa2
yummygyudon <bang2brew@gmail.com>
2022-12-11 12:45:35 +0900

    FEAT(view$OutputView) : 자판기 동전 현황 출력 메서드 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,4 +1,27 @@
 package vendingmachine.view;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringJoiner;
+import vendingmachine.model.Coin;
+import vendingmachine.view.util.Unit;
+
 public class OutputView {
+
+    public void printCoinStatus(final Map<Coin, Integer> coinTable){
+        for(Entry<Coin, Integer> eachCoinStatus : coinTable.entrySet()){
+            StringJoiner status = new StringJoiner(Unit.SEPARATE_UNIT.getUnit());
+            Coin coin = eachCoinStatus.getKey();
+            int coinCount = eachCoinStatus.getValue();
+
+            status.add(coin.getName());
+            status.add(Integer.toString(coinCount));
+
+            printMessage(status.toString());
+        }
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
 }
