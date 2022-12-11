fc6d1fdbe396f626292a56e88b78128ca1593392
juno-junho <ssmm0205@naver.com>
2022-12-11 13:31:45 +0900

    feat(InputView): 상품의 정보를 입력받는 메서드 구현
    readProductInfo 통해 info를 Product 리스트로 변환

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,15 +1,41 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.Product;
 import vendingmachine.domain.VendingMachine;
 import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+import java.util.List;
 
 public class VendingMachineController {
 
     public void run() {
         InputView inputView = new InputView();
-        int money = inputView.readVendingMachineMoney();
-        VendingMachine machine = new VendingMachine(money);
-        machine.generateRandomCoin();
+        OutputView outputView = new OutputView();
+//        int money = inputView.readVendingMachineMoney();
+        VendingMachine machine = new VendingMachine(1000);
+        machine.convertMoneyToRandomCoin();
+        System.out.println("money : " + machine.getMoney());
+        System.out.println("holdingMoney : " + machine.getHoldingMoney());
+
+        outputView.printMachineCoin(machine.getHoldingMoney());
+
+        List<Product> products = inputView.readProductInfo();
+
+
+
+    }
+
+    public static void main(String[] args) {
+        /*VendingMachine machine = new VendingMachine(1000);
+        machine.convertMoneyToRandomCoin();
+        int money = machine.getMoney();
+        System.out.println("money = " + money);
+        Map<Coin, Integer> holdingMoney = machine.getHoldingMoney();
+        System.out.println("holdingMoney = " + holdingMoney);*/
+
+        VendingMachineController controller = new VendingMachineController();
+        controller.run();
 
     }
 }
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,8 +1,12 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.domain.Product;
 import vendingmachine.domain.Validator;
 import vendingmachine.utils.Util;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
@@ -11,5 +15,17 @@ public class InputView {
         int money = Util.convertStringToInt(Console.readLine());
         Validator.validateMachineMoney(money);
         return money;
+    }
+
+    public List<Product> readProductInfo() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        String productListInfo = Console.readLine();
+        Validator.validateProductFormat(productListInfo);
+        return Util.convertProductListToProduct(productListInfo);
+    }
+
+    public static void main(String[] args) {
+        String productInfo = "[콜라,1500,20];[사이다,1000,10]";
+        Arrays.stream(productInfo.split(";")).forEach(System.out::println);
     }
 }
