944f34993aa871f7108b49f8dc2513b43c533f5d
juno-junho <ssmm0205@naver.com>
2022-12-11 11:50:45 +0900

    feat(domain): 랜덤한 금액을 생성하는 기능 구현

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,14 +1,60 @@
 package vendingmachine.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.Coin;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class VendingMachine {
 
     private int money;
 
+    private final Map<Coin, Integer> holdingMoney = new HashMap<>();
+    private final List<Integer> coins = Arrays.stream(Coin.values()).map(Coin::getAmount).collect(Collectors.toList());
+
     public VendingMachine(int money) {
         this.money = money;
+        setHoldingMoney();
     }
 
-    public void generateRandomCoin() {
+    private void setHoldingMoney() {
+        Arrays.stream(Coin.values()).forEach(coin -> holdingMoney.put(coin, 0));
+    }
 
+    public void convertMoneyToRandomCoin() {
+        while (money != 0) {
+            int generatedRandomCoin = generatedRandomCoin();
+            if (cannotConvertMoneyToCoin(generatedRandomCoin)) continue;
+            Coin coin = Coin.convertMachingCoint(generatedRandomCoin);
+            putCoinToMap(coin);
+            money -= generatedRandomCoin;
+        }
+    }
+
+    private boolean cannotConvertMoneyToCoin(int generatedRandomCoin) {
+        if (generatedRandomCoin > money){
+            return true;
+        }
+        return false;
+    }
+
+    private int generatedRandomCoin() {
+        return Randoms.pickNumberInList(coins);
+    }
+
+    private void putCoinToMap(Coin coin) {
+        holdingMoney.put(coin, holdingMoney.get(coin) + 1);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public Map<Coin, Integer> getHoldingMoney() {
+        return holdingMoney;
+    }
+
+    public static void main(String[] args) {
     }
 }
