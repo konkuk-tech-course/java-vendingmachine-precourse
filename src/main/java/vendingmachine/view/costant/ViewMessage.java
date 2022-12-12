c9c4c87238bbaff7e7832bf4e117f8e2f5484ad1
Life is a ninanino <tvxqinak@naver.com>
2022-12-12 16:50:02 +0900

    style : 클래스 이름 변경

--- src/main/java/vendingmachine/view/costant/ViewMessage.java
+++ src/main/java/vendingmachine/view/costant/ViewMessage.java
@@ -1,6 +1,6 @@
 package vendingmachine.view.costant;// @ author ninaaano
 
-public enum Message {
+public enum ViewMessage {
     INPUT_MACHINE_GET_COINS("\n자판기가 보유하고 있는 금액을 입력해 주세요."),
     INPUT_INSERT_PRODUCT("\n상품명과 가격, 수량을 입력해 주세요."),
     INPUT_INSERT_COINS("\n투입 금액을 입력해 주세요."),
@@ -13,7 +13,7 @@ public enum Message {
     private String message;
 
 
-    Message(String message) {
+    ViewMessage(String message) {
         this.message = message;
     }
 
