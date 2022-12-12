57f1964cd243261786b1e06a7f7d1302c1487557
Life is a ninanino <tvxqinak@naver.com>
2022-12-12 16:13:36 +0900

    feat : TreeMap에 Coin의 amount를 내림차순으로 비교하는 기능

---
+++ src/main/java/vendingmachine/domain/CoinStorage.java
@@ -0,0 +1,22 @@
+package vendingmachine.domain;// @ author ninaaano
+
+import vendingmachine.util.supporter.MapSupporter;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class CoinStorage {
+    public final SortedMap<Coin,Integer> coin = new TreeMap<>((c1, c2) -> c2.getAmount() - c1.getAmount());
+    private int inputCoinAmount;
+
+
+    public SortedMap<Coin, Integer> getCoins() {
+        return this.coin;
+    }
+
+    public void addCoins(SortedMap<Coin, Integer> coins) {
+        for(Coin coin : coins.keySet()){
+            MapSupporter.increaseCoinCount(this.coin,coin,0,coins.get(coin));
+        }
+    }
+}
