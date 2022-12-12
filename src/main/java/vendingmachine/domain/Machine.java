c16217499d33d02620f0b8bdd8c20369acfa40c6
Life is a ninanino <tvxqinak@naver.com>
2022-12-12 16:20:14 +0900

    feat : Coin을 가져와 Storage로 넘겨준다

---
+++ src/main/java/vendingmachine/domain/Machine.java
@@ -0,0 +1,18 @@
+package vendingmachine.domain;// @ author ninaaano
+
+import java.util.Map;
+import java.util.SortedMap;
+
+public class Machine {
+
+    CoinStorage coinStorage = new CoinStorage();
+
+
+    public SortedMap<Coin, Integer> getCoins() {
+        return coinStorage.getCoins();
+    }
+
+    public void addCoins(SortedMap<Coin,Integer> coins) {
+        coinStorage.addCoins(coins);
+    }
+}
